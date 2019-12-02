ef3537b288291fcafc6c5b6a07f77de9a73e2124
TiagoQuinteros <38963262+TiagoQuinteros@users.noreply.github.com>
2019-12-02 12:11:09 -0300

    se agrega sanitizacion de entradas (solo permite las letras ATCGatcg)

--- src/main/java/com/SMSLATAM/app/AdnRequestApi.java
+++ src/main/java/com/SMSLATAM/app/AdnRequestApi.java
@@ -1,15 +1,18 @@
 package com.SMSLATAM.app;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName(value = "adn")
 public class AdnRequestApi {
-	
+
 // TODO: how to make this work
 //	@Pattern(regexp = "[ATCGatcg]*$", message = "Una base nitrogenada solo pueden ser de tipo ATCG")
 	@NotNull
@@ -22,6 +25,20 @@ public class AdnRequestApi {
 
 	public void setDna(String[] dna) {
 		this.dna = dna;
+	}
+
+	public Boolean validate() {
+		Boolean validate = true;
+		for (@NotNull
+		String gen : this.dna) {
+			Pattern pat = Pattern.compile("[ATCGatcg]*$");
+			Matcher mat = pat.matcher(gen);
+			if (!mat.matches())
+				validate = false;
+
+		}
+		return validate;
+
 	}
 
 }
--- src/main/java/com/SMSLATAM/app/rest/MutantDetectorController.java
+++ src/main/java/com/SMSLATAM/app/rest/MutantDetectorController.java
@@ -1,7 +1,8 @@
 package com.SMSLATAM.app.rest;
 
 import javax.validation.Valid;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -29,7 +30,9 @@ public class MutantDetectorController {
 	@PostMapping(path = "/mutant/", produces = {
 			MediaType.APPLICATION_JSON_VALUE }, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> isMutant(@RequestBody(required = true) @Valid AdnRequestApi dna) {
-
+		if (!dna.validate())
+			return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+		
 		if (mutantDetectorService.isMutant(dna.getDna())) {
 			return new ResponseEntity<Object>(HttpStatus.OK);
 		} else {
